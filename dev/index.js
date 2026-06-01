cfdad208984f4a4301963619df3f4c5e5db21201
Joe Warren <hungryjoe.warren@gmail.com>
2026-06-01 16:32:29 +0100

    fix prod build

--- dev/index.js
+++ dev/index.js
@@ -1,18 +1,3 @@
-import amongusDefault from 'bundle-text:./Amongus-normal.svg';
-window.amongusDefault=amongusDefault;
-import amongusDead from 'bundle-text:./Amongus-deadbody.svg';
-window.amongusDead=amongusDead;
-import amongusImpostor from 'bundle-text:./Amongus-impostor.svg';
-window.amongusImpostor=amongusImpostor;
-import amongusThumbsup from 'bundle-text:./Amongus-thumbsup.svg';
-window.amongusThumbsup=amongusThumbsup
-import github from 'bundle-text:./github.svg';
-window.github = github
-import mastodon from 'bundle-text:./mastodon.svg';
-window.mastodon = mastodon
-import bsky from 'bundle-text:./bsky.svg';
-window.bsky = bsky
-import intro from 'bundle-text:./intro-image.svg';
-window.intro = intro
-
-require("../output/Main/index.js").main();
+window.onload = function(){
+    require("../output/Main/index.js").main();
+};
--- src/App/Svg.js
+++ src/App/Svg.js
@@ -1,9 +1,11 @@
-amongusDefault = window.amongusDefault
-amongusDead=window.amongusDead;
-amongusImpostor=window.amongusImpostor;
-amongusThumbsup=window.amongusThumbsup;
-github = window.github;
-mastodon = window.mastodon;
-bsky = window.bsky;
-intro = window.intro;
+// NB: this FFI file is copied to output/App.Svg/foreign.js at build time, so
+// these paths are resolved relative to output/App.Svg/, not src/App/.
+import amongusDefault from 'bundle-text:../../src/App/svgs/Amongus-normal.svg';
+import amongusDead from 'bundle-text:../../src/App/svgs/Amongus-deadbody.svg';
+import amongusImpostor from 'bundle-text:../../src/App/svgs/Amongus-impostor.svg';
+import amongusThumbsup from 'bundle-text:../../src/App/svgs/Amongus-thumbsup.svg';
+import github from 'bundle-text:../../src/App/svgs/github.svg';
+import mastodon from 'bundle-text:../../src/App/svgs/mastodon.svg';
+import bsky from 'bundle-text:../../src/App/svgs/bsky.svg';
+import intro from 'bundle-text:../../src/App/svgs/intro-image.svg';
 export { amongusDefault, amongusDead, amongusImpostor, amongusThumbsup, github, mastodon, bsky, intro };
